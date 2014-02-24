cf3c3773603b2b3e29b17fb407680f4f7ff7444c
Sami Vaisanen <sami.vaisanen@symbio.com>
2014-02-24 18:20:21 +0800

    Speedometer using exponential moving average

--- engine/speedometer.h
+++ engine/speedometer.h
@@ -22,14 +22,65 @@
 
 #pragma once
 
+#include <chrono>
+#include <cstddef>
+#include <cassert>
+#include "stopwatch.h"
+
 namespace newsflash
 {
     // track data transfer speeds.
     class speedometer 
     {
     public:
+        typedef std::chrono::steady_clock clock_t;
+        typedef clock_t::time_point point_t;
+
+        speedometer() : speed_(0)
+        {}
+
+        void start()
+        {
+            stamp_ = clock_t::now();
+        }
+
+        // submit a sample. samples should be submitted
+        // between calls to start() and end().
+        void submit(const std::size_t bytes)
+        {
+            assert(stamp_ != point_t());
+
+            const auto now = clock_t::now();
+            const auto ms  = std::chrono::duration_cast<std::chrono::milliseconds>(now - stamp_);
+
+            // time elapsed for the sample bytes as a fractional second.
+            const double s = ms.count() / 1000.0;
+
+            // this is the last sample speed.
+            const double bps = bytes / s;
+
+            // update the current average speed.
+            // http://stackoverflow.com/questions/2779600/how-to-estimate-download-time-remaining-accurately
+            const double SMOOTHING_FACTOR = 0.05;
+            speed_ = SMOOTHING_FACTOR * bps + (1 - SMOOTHING_FACTOR) * speed_;
+            stamp_ = now;
+        }
+
+        void end()
+        {
+            speed_ = 0.0;
+            stamp_ = point_t();
+        }
+
+        // get the current speed in bytes per second.
+        double bps() const
+        {
+            return speed_;
+        }
 
     private:
+        point_t stamp_;
+        double speed_;
     };
 
 } // newsflash
---
+++ engine/unit_test/unit_test_speedometer.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2014 Sami Väisänen, Ensisoft 
+//
+// http://www.ensisoft.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#include <boost/test/minimal.hpp>
+#include <thread>
+#include <functional>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "../speedometer.h"
+
+void produce_sample(int num_samples, int sample_bytes, newsflash::speedometer& meter)
+{
+    std::srand(std::time(NULL));
+    meter.start();
+
+    for (int i=0; i<num_samples; ++i)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        const int bytes = 0.8 * sample_bytes + (rand() % (int)(sample_bytes * 0.2));
+
+        assert(bytes >= (int)0.8 * sample_bytes && 
+            bytes <= sample_bytes);
+
+        meter.submit(bytes);
+    }
+}
+
+
+void test_multiple_thread()
+{
+    newsflash::speedometer meter;
+
+    std::thread thread(std::bind(produce_sample, 100, 128, std::ref(meter)));
+
+    // read the meter. 
+    for (int i=0; i<200; ++i)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << meter.bps() << std::endl;
+    }
+
+    thread.join();
+}
+
+
+int test_main(int, char*[])
+{
+    test_multiple_thread();
+
+    return 0;
+}
