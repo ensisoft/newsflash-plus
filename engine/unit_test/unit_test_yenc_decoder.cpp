3b81dd8c375096e252df871d19abda9c040b32d7
Sami Väisänen <samiv@ensisoft.com>
2014-03-03 00:23:22 +0800

    Unfinished yenc decoder test code

--- engine/unit_test/unit_test_yenc_decoder.cpp
+++ engine/unit_test/unit_test_yenc_decoder.cpp
@@ -20,26 +20,51 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.            
 #include <boost/test/minimal.hpp>
+#include <boost/crc.hpp>
+#include <vector>
+#include <cstring>
 #include "../yenc_single_decoder.h"
 #include "../yenc_multi_decoder.h"
+#include "unit_test_common.h"
 
-void decoder_write(const void* data, std::size_t size, std::size_t offset)
-{}
+struct tester 
+{
+    std::vector<char> binary;
+    std::vector<std::string> errors;
+    newsflash::decoder::info info;
 
-void decoder_info(const newsflash::decoder::info& info)
-{}
+    tester(newsflash::decoder& dec)
+    {
+        dec.on_write = std::bind(&tester::decoder_write, this, 
+            std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+        dec.on_info = std::bind(&tester::decoder_info, this,
+            std::placeholders::_1);
+        dec.on_error = std::bind(&tester::decoder_error, this,
+            std::placeholders::_1);
+    }
 
-void decoder_error(const std::string& err)
-{}
+    void decoder_write(const void* data, std::size_t size, std::size_t offset)
+    {
+        if (binary.size() < size + offset)
+            binary.resize(size + offset);
+        std::memcpy(&binary[offset], data, size);
+    }
 
+    void decoder_info(const newsflash::decoder::info& info)
+    {
+        this->info = info;
+    }
+    void decoder_error(const std::string& err)
+    {
+        errors.push_back(err);
+    }
+};
 
 void test_single_success()
 {
     newsflash::yenc_single_decoder yenc;
-    yenc.on_write = decoder_write;
-    yenc.on_info  = decoder_info;
 
-    //yenc.decode()
+    tester test(yenc);
 }
 
 void test_single_broken()
