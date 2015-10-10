97bb137f40bfcd342f4b77d5bcfe667a789b457b
Sami Väisänen <samiv@ensisoft.com>
2015-10-10 20:42:07 +0800

    Version bump to 4.1.1

--- config.h
+++ config.h
@@ -131,8 +131,8 @@
 
 // these are macros so that they can be embedded
 // inside string literals easily for exaple L"yadi yadi" VERSION_STRING "yadi yadi"
-//#define NEWSFLASH_VERSION   "4.1.1a1"
-#define NEWSFLASH_VERSION   "master" // remember to edit!
+#define NEWSFLASH_VERSION   "4.1.1"
+//#define NEWSFLASH_VERSION   "master" // remember to edit!
 #define NEWSFLASH_COPYRIGHT "Copyright (c) Sami V\303\244is\303\244nen 2005-2015"
 #define NEWSFLASH_WEBSITE   "http://www.ensisoft.com"
 
@@ -142,4 +142,4 @@
 #  define NEWSFLASH_TITLE "Newsflash Plus DEBUG"
 #endif  
 
-#define NEWSFLASH_DEBUG
+//#define NEWSFLASH_DEBUG
